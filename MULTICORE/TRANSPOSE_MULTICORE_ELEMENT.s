4338f48172ef5f9f5757db065efcfab45b5c02c3
sthiranjaya <sthiranjaya@gmail.com>
2020-10-12 13:31:10 -0400

    element by element

--- MULTICORE/TRANSPOSE_MULTICORE_ELEMENT.s
+++ MULTICORE/TRANSPOSE_MULTICORE_ELEMENT.s
@@ -18,55 +18,65 @@
 ;     00000100-CURR
 
 
-//////NoC = 0000 0100     -number of cores is 4
-//////CoreID = xxxx xxxx  -ID of the core starting from 0000 0000 
-/////element no starts from 0000 0000
+# NoC = 0000 0100     -number of cores is 4
+# CoreID = xxxx xxxx  -ID of the core starting from 0000 0000 
+# element no starts from 0000 0000
 
 LDAC 0x00       // AC <-- NoC 
 MOVEAC R        // R <-- AC    ...(R <- NoC)
-LDAC 0x01   //no.of rows in A = no.of rows in C
+LDAC 0x01       //no.of rows in A = no.of rows in C
 MOVEAC N
-
-mloop1:
-
-LDAC 0x02   //no.of columns in A = no.of rows in B
+LDAC 0x02  
 MOVEAC M
-LDAC 0x03   //no.of columns in B = no.of columns in C - (P is loaded to AC)
-MUL ROW     // AC <-- AC * ROW ... [P*ROW]
-ADD COL     // AC <-- AC+ COL  ... [P*ROW + COL] -- Element No. is in AC
+LDAC 0x03
+MOVEAC P 
 
-MOD R        // Element no. is mod by the No. of cores -- answer is from 0000 0000 to (NOC - 1)****
-XOR CoreID      // AC <-- AC (+) CoreID.. if equal Z=1 else Z=0 
-JUMPNZ mend      // if Z = 0 no operation ****
+loop1:
+    MOVETOAC N 
+    MUL P 
+    DIV CoreID   #(N*P)/CoreID  if (N*P)<CoreID ==> AC=0   if AC==0 Z==1
+    JUMPZ end
 
-LDAC 0x04       //Starting address of Mat A in memory
-MOVEAC STA
-MOVETOAC M
-MUL ROW
-ADD STA       // AC <-- STA + (M*ROW)
-MOVEAC STA    // STA <-- STA + (M*ROW) .... STA is pointed at the first element of the respective row 
-         
-LDAC 0x05       //Starting address of Mat B in memory
-MOVEAC STB
-MOVETOAC M
-MUL COL
-ADD STB       // AC <-- STB + (M*COL)
-MOVEAC STB    // STB <-- STB + (M*COL) .... STB is pointed at the first element of the respective colomn
+    MOVETOAC CoreID 
+    DIV P 
+    MOVEAC ROW  #ROW <-- CoreID/P
+
+    MOVETOAC CoreID
+    MOD P 
+    MOVEAC COL  #COL <-- CoreID%P
+
+    LDAC 0x04       //Starting address of Mat A in memory
+    MOVEAC STA
+    MOVETOAC M
+    MUL ROW
+    ADD STA       // AC <-- STA + (M*ROW)
+    MOVEAC STA    // STA <-- STA + (M*ROW) .... STA is pointed at the first element of the respective row 
+
+    LDAC 0x05       //Starting address of Mat B in memory
+    MOVEAC STB
+    MOVETOAC M
+    MUL COL
+    ADD STB       // AC <-- STB + (M*COL)
+    MOVEAC STB    // STB <-- STB + (M*COL) .... STB is pointed at the first element of the respective colomn
         
-LDAC 0x06       //Starting address of Mat C in memory
-MOVEAC STC
+    LDAC 0x06       //Starting address of Mat C in memory
+    MOVEAC STC
+    MOVETOAC CoreID
+    MULV 2
+    ADD STC
+    MOVEAC STC    #STC<--STC+(CoreID*2)
 
-RESET CURR       // CURR <-- 0
+    RESET CURR       // CURR <-- 0
 
- mloop2:
+    loop2:
           MOVETOAC CURR   //AC <-- CURR
           XOR M           //AC <-- AC ^ M
           JUMPZ mstore    //if Z==1, jump to mstore  ..... compares CURR and M
 
-          LDAC STA      //AC <-- value at STA (address of current cell in A)
+          LDAC STA        //AC <-- value at STA (address of current cell in A)
           MOVEAC AVAL     //AVAL <-- AC 
 
-          LDAC STB      //AC <-- value at STB (address of current cell in B)
+          LDAC STB        //AC <-- value at STB (address of current cell in B)
             
           MUL AVAL        //AC <-- AC * AVAL     (result is BVAL *AVAl)
           ADD SUM         //AC <-- AC + SUM 
@@ -78,15 +88,16 @@ RESET CURR       // CURR <-- 0
 
           JUMP mloop2
 
-mstore:
+
+    mstore:
           MOVETOAC SUM    //AC <-- SUM
           STAC STC     //M[STC] <--- AC      (STC is the current cell in C)
 
-MOVETOAC R    //move NoC to AC
-MULV S        // AC <-- NoC * S 
-ADD STC       // AC <-- STC + (NoC * S)
-MOVEAC STC
-
-JUMP mloop1
+    MOVETOAC CoreID
+    ADD R 
+    MOVEAC CoreID  #CoreID = CoreID + NoC
+    JUMP loop1
 
 
+end:
+    NOP
